83db85b5f4d2cf56395a5e75b56e916956246ebc
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 19:33:28 +0530

    Route testing

--- server/routes/update.js
+++ server/routes/update.js
@@ -10,26 +10,27 @@ const Items = require("../models/Items")
 // @route   POST user/add
 // @desc    Adding item(s) 
 // @access  Public
-let arr = [ { item: 'Arul', money: '100' }, { item: 'Samantha', money: '500' }, { item: 'Boby', money: '10' }]
-router.post("/one", (arr,res)=>{
-    const {item, money} =req.body
-    console.log(req.body)
-        Items.findOne({ item })
-        .then( () =>{
-            if(true){
-                const newEntry = new Items({
-                    item,
-                    money
-                })
-                newEntry.save()
-                .then(data =>{
-                    console.log('Successfully included')
-                    console.log(data)
-                })
-                .catch(err => console.log(err))
-            }
-        })
-        .then(res.send('Added'))
+// let arr = req.body
+router.post("/one", (req,res)=>{
+    const {object} =req.body
+    console.log(object.value)
+    res.send(object.value)
+        // Items.findOne({ item })
+        // .then( () =>{
+        //     if(true){
+        //         const newEntry = new Items({
+        //             item,
+        //             money
+        //         })
+        //         newEntry.save()
+        //         .then(data =>{
+        //             console.log('Successfully included')
+        //             console.log(data)
+        //         })
+        //         .catch(err => console.log(err))
+        //     }
+        // })
+        // .then(res.send('Added'))
 })
 
 
